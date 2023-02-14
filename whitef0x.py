0b2ab25b7b1794f8e5e795bcf415fc58058097a0
Khaleel Khan <118879404+ikhaleelkhan@users.noreply.github.com>
2023-02-14 22:13:27 +0530

    Update whitef0x.py

--- whitef0x.py
+++ whitef0x.py
@@ -1,56 +1,71 @@
 #!/usr/bin/env python3
 
 import argparse
+import concurrent.futures
 import dns.resolver
-import socket
+import requests
 import sys
-
+import os
 
 def get_subdomains(domain, wordlists):
+    subdomains = set()
     for wordlist in wordlists:
         with open(wordlist, 'r') as f:
             for line in f:
-                subdomain = line.strip()
-                if subdomain == "":
-                    continue
-                yield f"{subdomain}.{domain}"
-
-
-def get_recursive_subdomains(domain, recursive, wordlists):
-    subdomains = set()
-    processed_subdomains = set()
-    subdomains.update(get_subdomains(domain, wordlists))
-    processed_subdomains.add(domain)
-    while recursive and len(subdomains) > 0:
-        subdomain = subdomains.pop()
-        if subdomain in processed_subdomains:
-            continue
-        processed_subdomains.add(subdomain)
-        try:
-            answers = dns.resolver.query(subdomain, 'A')
-            for rdata in answers:
-                ip = rdata.address
-                subdomains.add((subdomain, ip))
-        except:
-            pass
+                subdomain = line.strip() + '.' + domain
+                subdomains.add(subdomain)
     return subdomains
 
+def get_recursive_subdomains(domain, level, wordlists):
+    if level == 0:
+        return set()
+    subdomains = get_subdomains(domain, wordlists)
+    recursive_subdomains = set()
+    for subdomain in subdomains:
+        try:
+            answers = dns.resolver.query(subdomain, 'A')
+            for answer in answers:
+                recursive_subdomains.add(subdomain)
+                recursive_subdomains.update(get_recursive_subdomains(answer.address, level-1, wordlists))
+        except:
+            pass
+    return recursive_subdomains
+
+def scan_subdomain(subdomain, scan_opts):
+    try:
+        response = requests.get(scan_opts['proto'] + '://' + subdomain, timeout=scan_opts['timeout'])
+        if response.status_code == 200:
+            print(subdomain)
+        if scan_opts['output']:
+            with open(scan_opts['output'], 'a') as f:
+                f.write(subdomain + '\n')
+    except:
+        pass
 
 def main():
-    parser = argparse.ArgumentParser(description="WhiteF0x - Sub Domain Enumeration Tool")
-    parser.add_argument("domain", help="The domain to enumerate subdomains for")
-    parser.add_argument("-r", "--recursive", help="Enable recursive subdomain enumeration", action="store_true")
-    parser.add_argument("-w", "--wordlists", help="Comma-separated list of wordlists to use", default="wordlists/common.txt")
-    parser.add_argument("-v", "--verbose", help="Enable verbose output", action="store_true")
+    parser = argparse.ArgumentParser(description='WhiteF0x Sub Domain Enumeration Tool')
+    parser.add_argument('domain', type=str, help='the domain to scan')
+    parser.add_argument('-w', '--wordlists', type=str, nargs='+', default=['subdomains.txt', 'common.txt'], help='the wordlists to use for subdomain enumeration')
+    parser.add_argument('-r', '--recursive', type=int, default=0, help='the number of levels of recursive subdomain enumeration')
+    parser.add_argument('-t', '--threads', type=int, default=10, help='the number of threads to use for scanning')
+    parser.add_argument('-p', '--proto', type=str, default='http', help='the protocol to use for scanning (http or https)')
+    parser.add_argument('-o', '--output', type=str, default=None, help='the file to output results to')
+    parser.add_argument('--timeout', type=int, default=5, help='the timeout in seconds for each HTTP request')
     args = parser.parse_args()
 
-    wordlists = args.wordlists.split(",")
-    subdomains = get_recursive_subdomains(args.domain, args.recursive, wordlists)
-    for subdomain, ip in subdomains:
-        print(f"{subdomain}\t{ip}")
-    if args.verbose:
-        print(f"Found {len(subdomains)} subdomains.")
+    if args.recursive > 0:
+        subdomains = get_recursive_subdomains(args.domain, args.recursive, args.wordlists)
+    else:
+        subdomains = get_subdomains(args.domain, args.wordlists)
 
+    scan_opts = {
+        'proto': args.proto,
+        'timeout': args.timeout,
+        'output': args.output,
+    }
 
-if __name__ == "__main__":
+    with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
+        executor.map(scan_subdomain, subdomains, [scan_opts]*len(subdomains))
+
+if __name__ == '__main__':
     main()
